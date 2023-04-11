b8509d88b79fefee555cb7d73cae2f035e82880d
Jaswanthsarma <129160638+Jaswanthsarma@users.noreply.github.com>
2023-04-11 13:54:04 +0530

    Add files via upload

---
+++ logical.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	bool result;
+	int a=20;
+	int b=13;
+	cout<<"(a>10)&&(b>5) ="<<((a>10)&&(b>5))<<endl;
+    cout<<"(a>10)||(b>5) ="<<((a>10)||(b>5))<<endl;
+    cout<<"!(a<10) ="<<!(a>10)<<endl;	
+}
+	
